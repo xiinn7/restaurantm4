56e6b6e53c067b24bf680ce6e4fad9848266fa40
陳榕馨 <ruby881017@gms.tku.edu.tw>
2021-06-19 21:39:27 +0800

    Adding initial files

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '6e86c1625fecabed669d381f335b07e8'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'd736a86d-cdf5-4762-b5a2-8563d00e26aa'
+};
